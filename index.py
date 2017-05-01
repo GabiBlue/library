fdd681f6d32f7ca7de8138875dc03d570466e706
Javora Gabor <javora.gabor@gmail.com>
2017-05-01 16:56:13 +0200

    Changed variable names.

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ import json
 
 user = ""
 
+
 @app.route('/')
 def index():
     return send_file('templates/index.html')
@@ -30,7 +31,7 @@ def login():
             user = u
             break
 
-    if (not user):
+    if not user:
         response = Response(status=401)
         return response
     else:
@@ -49,16 +50,16 @@ def logout():
 @app.route('/userSettings', methods=['POST'])
 def user_settings():
     global user
-    requestUser = request.json
-    modifiedUser = User.query.get(user.id)
-    if modifiedUser.password == requestUser['currentPassword']:
-        if requestUser['newPassword'] != requestUser['confirmNewPassword']:
+    requested_user = request.json
+    modified_user = User.query.get(user.id)
+    if modified_user.password == requested_user['currentPassword']:
+        if requested_user['newPassword'] != requested_user['confirmNewPassword']:
             response = Response(status=409)
             return response
-        modifiedUser.name = requestUser['name']
-        modifiedUser.password = requestUser['newPassword']
+        modified_user.name = requested_user['name']
+        modified_user.password = requested_user['newPassword']
         db.session.commit()
-        user = modifiedUser
+        user = modified_user
         response = Response(status=200)
         return response
     else:
@@ -114,21 +115,22 @@ def add_book():
             response = Response(status=409)
             return response
 
-    new_book = Book(author=book['author']['name'], title=book['title'], genre=book['genre']['genre'], quantity=0, available=0)
+    new_book = Book(author=book['author']['name'], title=book['title'], genre=book['genre']['genre'],
+                    quantity=0, available=0)
     db.session.add(new_book)
     db.session.commit()
     response = Response(status=200)
     return response
 
 
-@app.route('/addBookInstance/<int:bookID>/<quantity>', methods=['POST'])
-def add_book_instance(bookID, quantity):
+@app.route('/addBookInstance/<int:book_id>/<quantity>', methods=['POST'])
+def add_book_instance(book_id, quantity):
     if quantity == 'undefined' or int(quantity) < 0:
         response = Response(status=409)
         return response
     books = Book.query.all()
     for b in books:
-        if b.id == bookID:
+        if b.id == book_id:
             b.quantity += int(quantity)
             b.available += int(quantity)
     db.session.commit()
@@ -139,17 +141,17 @@ def add_book_instance(bookID, quantity):
 @app.route('/requests', methods=['GET'])
 def get_requests():
     request_result = Request.query.all()
-    requests = [request.to_dict() for request in request_result]
+    requests = [req.to_dict() for req in request_result]
     response = Response(response=json.dumps(requests), status=200, mimetype='application/json')
     return response
 
 
-@app.route('/requestBook/<int:bookID>', methods=['POST'])
-def request_book(bookID):
+@app.route('/requestBook/<int:book_id>', methods=['POST'])
+def request_book(book_id):
     books = Book.query.all()
     book = {}
     for b in books:
-        if b.id == bookID:
+        if b.id == book_id:
             book = b
             break
     new_request = Request(author=book.author, title=book.title, user=user.username)
